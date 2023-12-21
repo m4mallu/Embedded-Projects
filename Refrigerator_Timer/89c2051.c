89ba6c076472cc24127047ad2e5270407a1ba9f0
Renjith Mangalath <65238086+m4mallu@users.noreply.github.com>
2023-12-20 22:59:02 -0500

    Add files via upload

---
+++ Refrigerator_Timer/89c2051.c
@@ -0,0 +1,51 @@
+#include<AT89x051.h>
+int i,j;
+ void delay1()
+{
+ TMOD=0x01;				//STARTING 16Bit TIMER				
+ 	for (i=0;i<=380;i++)
+	{
+		for(j=0;j<=380;j++)
+ 		{
+  			TL0=0xAF;
+  			TH0=0x3C;
+  			TR0=1;
+  			while(TF0==0);
+  			TR0=0;
+  			TF0=0;
+		}
+ 	}
+}
+ void delay2()
+{ 
+ TMOD=0x01;
+ 	for (j=0;j<=144;j++)
+	 {
+  		for(i=0;i<=144;i++)
+ 		{
+			TL0=0XAF;
+  			TH0=0x3C;
+  			TR0=1;
+  			while(TF0==0);
+  			TR0=0;
+  			TF0=0;
+		}
+ 	}
+}
+void main()
+{  
+P1=0;
+P3=0;
+ while(1)
+ {
+ 	P1_7 =1;
+ 	P1_6 =1;
+	P1_5 =1;
+		delay1();
+	P1_7 =0;
+ 	P1_6 =0;
+	P1_5 =0;
+		delay2();                      
+ }
+}
+
---
+++ Refrigerator_Timer/89c2051_Sample.c
@@ -0,0 +1,46 @@
+#include<AT89x051.h>
+P2=0;
+P0=0;
+void delay1()
+{
+ 	int i;
+ 	TMOD=0x01;
+ 	for (i=1;i<=200;i++)	// 10 sec
+ 	{
+  		TL0=0xAF;
+  		TH0=0x3C;
+  		TR0=1;
+  		while(TF0==0);
+  		TR0=0;
+  		TF0=0;
+ 	}
+}
+ void delay2()
+{ 
+	int j;
+ 	TMOD=0x01;
+ 	for (j=0;j<=100;j++)	// 5 sec
+ 	{
+  		TL0=0XAF;
+  		TH0=0x3C;
+  		TR0=1;
+  		while(TF0==0);
+  		TR0=0;
+  		TF0=0;
+ 	}
+}
+void main()
+{
+	P1=0;
+	P3=0;
+while(1)
+ {
+ 	P1_0 =1;
+ 	P1_1 =1;
+		delay1();
+	P1_0 =0;
+ 	P1_1 =0;
+ 		delay2();
+ }
+}
+
